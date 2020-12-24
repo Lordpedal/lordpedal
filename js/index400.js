20d4d1b2102c329aa4dfff4dac4dda7317df1ef0
Lordpedal <jm.lorente@gmail.com>
2020-12-24 12:10:25 +0100

    Actualizacion jue dic 24 12:10:25 CET 2020

--- js/index400.js
+++ js/index400.js
@@ -5,11 +5,11 @@
 $(function(){
   $('.text').typed({
     strings: [
-      "Oops! Pareces perdido. <br /> ^950" +
-      "Te voy a echar un cable. <br /> ^950" +
-      "Nos hemos traslado de dominio, vas a ser redigirido a la nueva web en unos segundos. <br /> ^950" +
+      "Oops! Pareces perdido. <br /> ^1000" +
+      "Te voy a echar un cable. <br /> ^1000" +
+      "Nos hemos traslado de dominio, vas a ser redigirido a la nueva web en unos segundos. <br /> ^1000" +
       " <br /> ^250" +
-      "<a href='https://lordpedal.github.io'>lordpedal.github.io</a> <br /> ^950" +
+      "<a href='https://lordpedal.github.io'>lordpedal.github.io</a> <br /> ^1000" +
       " <br /> ^250" +
       " <br /> ^250" +
       "... la culminación de un fin es el comienzo de una realidad."
