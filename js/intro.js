4950dfde743f871ab19e2494a87eae0ac87d1f2a
Lordpedal <jm.lorente@gmail.com>
2021-01-03 18:27:08 +0100

    Actualización dom ene  3 18:27:08 CET 2021

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,5 @@
 document.write('<div class="navbar">'
-+ '<a href="index.html">Inicio</a>'
-+ '<a href="https://github.com/Lordpedal/lordpedal"target="_blank">Código en GitHub</a>'
-+ '<a href="https://gitlab.com/Lordpedal"target="_blank">GitLab Pages</a>'
++ '<a href="index.html">Inicio</a>' 
++ '<a href="https://github.com/Lordpedal/lordpedal"target="_blank">Código en GitHub</a>' 
++ '<a href="https://gitlab.com/Lordpedal"target="_blank">GitLab Pages</a>' 
 + '</div>');
